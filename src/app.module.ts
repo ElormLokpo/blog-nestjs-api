f66e3919717bab4549e9fe79bbc6d26460e34844
ElormLokpo <benedictkomla@gmail.com>
2023-05-30 07:47:39 +0000

    strictly backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({
       isGlobal:true
     }),
-    MongooseModule.forRoot(''),
+    MongooseModule.forRoot('mongodb+srv://benedict:benedict@cluster0.gstdo9d.mongodb.net/blognest'),
     BlogModule,
     ContentModule,
     CommentsModule,
